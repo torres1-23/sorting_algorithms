b2aec24f3247e48a4250509f43344b91a51bbc65
torres1-23 <70782680+torres1-23@users.noreply.github.com>
2021-01-28 22:31:12 +0000

    Add file with bubble sirtung algorithm

---
+++ 0-bubble_sort.c
@@ -0,0 +1,28 @@
+#include "sort.h"
+
+/**
+ * bubble_sort - sorts an array of integers in ascending order using
+ * the Bubble sort algorithm.
+ * @array: Pointer to array to sort.
+ * @size: Size of array.
+ */
+
+void bubble_sort(int *array, size_t size)
+{
+	unsigned int i, j;
+	int tmp = 0;
+
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < size - i - 1; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				tmp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = tmp;
+				print_array(array, size);
+			}
+		}
+	}
+}
