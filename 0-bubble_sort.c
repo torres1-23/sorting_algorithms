0d5bd3ad1e7ae26cba780e133f4547831d322c19
torres1-23 <70782680+torres1-23@users.noreply.github.com>
2021-01-28 23:10:44 +0000

    Add none array argument handling

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -12,6 +12,8 @@ void bubble_sort(int *array, size_t size)
 	unsigned int i, j;
 	int tmp = 0;
 
+	if (!array || size < 2)
+		return;
 	for (i = 0; i < size; i++)
 	{
 		for (j = 0; j < size - i - 1; j++)
