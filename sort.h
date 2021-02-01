ea891ca814cdbc77b36be1c832598acbc2e84429
torres1-23 <70782680+torres1-23@users.noreply.github.com>
2021-02-01 20:52:07 +0000

    Updated header file

--- sort.h
+++ sort.h
@@ -21,5 +21,7 @@ typedef struct listint_s
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
 void bubble_sort(int *array, size_t size);
+void insertion_sort_list(listint_t **list);
+void swap_nodes(listint_t **head, listint_t *nxt, listint_t *prev);
 
 #endif /* _SORT_H */
